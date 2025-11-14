0778c03402d111c7b001a2deaea50802509e7020
SeppCel <fdl.edrd@gmail.com>
2025-11-14 17:20:44 +0100

    aggiunta di etichetta energia segnale grafico 1, etichetta correlazione x-y grafico 4

--- hwCode.py
+++ hwCode.py
@@ -71,8 +71,12 @@ y_n = np.convolve(pressioni, h_x_reale, mode="same")
 
 #Esercizio 2 punto b
 #autocorrelazione segnali x e y
-x_corr = int(np.correlate(pressioni, pressioni))
-y_corr = int(np.correlate(y_n, y_n))
+
+x1N = pressioni - np.mean(pressioni)
+y1N = y_n - np.mean(y_n)
+r_xx = np.correlate(x1N, x1N, mode='full')
+r_yy = np.correlate(y1N, y1N, mode='full')
+lag = np.arange(-len(pressioni) + 1, len(pressioni))
 
 
 ###grafici in una sola finestra###
@@ -80,56 +84,78 @@ y_corr = int(np.correlate(y_n, y_n))
 #finestra unica
 fig, axs = plt.subplots(3, 2, figsize=(15, 12))
 
-# Grafico 1
-axs[0, 0].plot(ore, pressioni, label='funzione', color='#1f77b4', linewidth=1)
+#GRAFICO 1---------------
+energia = np.sum(pressioni**2)
+axs[0, 0].plot(ore, pressioni, label='Funzione', color='#1f77b4', linewidth=1)
 axs[0, 0].axhline(valore_medio(pressioni), linestyle='--', color='darkred', label=f'Valore Medio: {valore_medio(pressioni):.2f}')
-#axs[0, 0].axhline(energia(pressioni), linestyle='--', color='darkred', label=f'Energia: {energia(pressioni):.2f}')
 axs[0, 0].set_xlim(min(ore), max(ore))
 axs[0, 0].set_ylim(min(pressioni), max(pressioni))
 axs[0, 0].set_xlabel("Orario")
 axs[0, 0].set_ylabel("Pressioni")
-axs[0, 0].set_title("Esercizio 1: Segnale Pressioni")
+axs[0, 0].set_title("Segnale Pressioni")
 axs[0, 0].grid(True, linestyle=':', alpha=0.5)
 axs[0, 0].legend()
+axs[0, 0].text(0.70, 0.10, f'Energia = {energia:.2f}', transform=axs[0,0].transAxes, 
+         bbox=dict(facecolor='white', edgecolor='black'))
 
 
-# Esercizio 2a
-axs[0, 1].plot(x_spostato, y_n, label='segnale filtrato y_n', color="#060002", linewidth=1)
+#GRAFICO 2a---------------
+axs[0, 1].plot(x_spostato, y_n, label='Segnale Filtrato y_n', color="#060002", linewidth=1)
 axs[0, 1].set_ylim(31,38)
-axs[0, 1].set_title("Esercizio 2a: Segnale filtrato (y_n) e segnale originale")
+axs[0, 1].set_title("Segnale filtrato (y_n) e segnale originale")
 axs[0, 1].set_xlabel("Campioni (spostati)")
 axs[0, 1].set_ylabel("Ampiezza")
 axs[0, 1].grid(True, linestyle=':', alpha=0.5)
 axs[0, 1].legend()
 
-axs[0, 1].plot(x_spostato, pressioni, label='funzione originale', color="#17C2D2", linewidth=0.5)
+axs[0, 1].plot(x_spostato, pressioni, label='Funzione Originale', color="#17C2D2", linewidth=0.5)
 axs[0, 1].set_xlabel("Campioni (spostati)")
 axs[0, 1].set_ylabel("Pressione")
 axs[0, 1].grid(True, linestyle=':', alpha=0.5)
 axs[0, 1].legend()
 
-axs[1, 0].plot(h_x_reale, label='filtro sinc', color="#FF5733", linewidth=1)
-axs[1,0].set_title("Filtro Sinc Applicato")
-axs[1,0].set_xlabel("Campioni")
-axs[1,0].set_ylabel("Ampiezza")
-axs[1,0].grid(True, linestyle=':', alpha=0.5)
-axs[1,0].legend()
+axs[1, 0].plot(h_x_reale, label='Filtro Sinc', color="#FF5733", linewidth=1)
+axs[1, 0].set_title("Filtro Sinc Applicato")
+axs[1, 0].set_xlabel("Campioni")
+axs[1, 0].set_ylabel("Ampiezza")
+axs[1, 0].grid(True, linestyle=':', alpha=0.5)
+axs[1, 0].legend()
 
-#es2b: Autocorrelazione X
-axs[1, 1].axhline(x_corr, linestyle='--', color='darkred', label=f'autocorrelazione x: {x_corr:.2f}')
+
+##GRAFICO 2B---------------
+#Autocorrelazione X
+axs[1, 1].plot(lag, r_xx, label='Autocorrelazione x', color='blue', linewidth=1)
 axs[1, 1].set_title("Autocorrelazione pressioni (Energia)")
 axs[1, 1].grid(True, alpha=0.3)
 axs[1, 1].legend()
 axs[1, 1].set_yticks([]) 
+#Autocorrelazione Y
+axs[1, 1].plot(lag,r_yy, label='Autocorrelazione y', color='orange', linewidth=1)
+axs[1, 1].grid(True, alpha=0.3)
+axs[1, 1].legend()
+axs[1, 1].set_yticks([]) 
+
+correlazione_xy = np.corrcoef(pressioni, y_n)[0, 1]
+testo_corr = f'Correlazione X e Y: {correlazione_xy:.4f}'
+axs[1, 1].text(0.95, 0.80,
+             testo_corr,  
+             transform=axs[1, 1].transAxes, 
+             fontsize=10,
+             verticalalignment='top',
+             horizontalalignment='right',
+             bbox=dict(boxstyle='round', facecolor='white', alpha=0.8))
 
 
-#es2b: Autocorrelazione Y
-axs[2, 0].axhline(y_corr, linestyle='--', color='darkred', label=f'autocorrelazione y: {y_corr:.2f}')
-axs[2, 0].set_title("Autocorrelazione filtro (Energia)")
-axs[2, 0].grid(True, alpha=0.3)
-axs[2, 0].legend()
-axs[2, 0].set_yticks([])
-
+#x_corr = int(np.correlate(pressioni, pressioni))
+#y_corr = int(np.correlate(y_n, y_n))
+#testo_energia = f'Energia X: {x_corr}\nEnergia Y: {y_corr}'
+#axs[1, 1].text(0.95, 0.95,
+#             testo_energia,  
+#             transform=axs[1, 1].transAxes, 
+#             fontsize=10,
+#             verticalalignment='top',
+#             horizontalalignment='right',
+#             bbox=dict(boxstyle='round', facecolor='white', alpha=0.8))
 
 #axs[2, 1].axis('off')
 
